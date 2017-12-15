1e11e36346043c3c10966c9f54e79ac31423c044
ms10596 <ms10596@gmail.com>
2017-12-16 01:36:42 +0200

    forgot something again

--- src/storeOwner.java
+++ src/storeOwner.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 import java.util.Vector;
-
 public class StoreOwner extends Person {
     protected StoreOwnerView storeOwnerView;
-
     public StoreOwner(String name, String email, String password) {
         super(name,email,password);
     }
@@ -12,6 +10,18 @@ class StoreOwnerController{
     protected Vector<Store>stores;
     public void addStore(Store store){
         stores.add(store);
+        Database.addStore(store);
+    }
+    public Store findStore(String storeName){
+        for(Store store:stores){
+            if(store.getName().equals(storeName)){
+                return store;
+            }
+        }
+        return null;
+    }
+    public void addSuggestion(String suggestion){
+        Database.addSuggestion(suggestion);
     }
 }
 class StoreOwnerView{
@@ -25,5 +35,25 @@ class StoreOwnerView{
         System.out.println("Enter store address (enter null in case of online stores): ");
         String address=cin.nextLine();
         storeOwnerController.addStore(new Store(name,type,address));
+    }
+    public void addProduct(){
+        Scanner cin=new Scanner(System.in);
+        System.out.print("Enter store name you would like to add product in it: ");
+        String storeName=cin.nextLine();
+        Store currentStore=null;
+        currentStore=storeOwnerController.findStore(storeName);
+        if(currentStore!=null){
+            currentStore.getStoreView().addProduct();
+        }
+        else {
+            System.out.println("Store not found");
+        }
+    }
+    public void suggestAddingProductToDatabase(){
+        Scanner cin=new Scanner(System.in);
+        System.out.println("Enter product name and a brief description\nAdmin should revise your suggestion within 24 hours.");
+        String suggestion=cin.nextLine();
+        storeOwnerController.addSuggestion(suggestion);
+        System.out.println("Your suggestion has been sent!");
     }
 }
